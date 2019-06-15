2a6d524e6f65fbfe9c27c0ae199ef51e64becf3d
AhmNouira <ahmnouira@gmail.com>
2019-06-15 20:36:38 +0100

    IoT_platfrom_flask
    v0.2

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 
+
+
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/testFlask'  #connection to mysql db
+    SQLALCHEMY_DATABASE_URI = 'mysql://bob:secret@192.168.1.7/testF'  #connection to mysql db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMINS = ['ahmnouira@gmail.com']
     
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,3 +11,15 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
+class Dht11(db.Model):
+    __tablename__ = 'dht11'
+    id = db.Column(db.BigInteger, primary_key=True)
+    temperature = db.Column(db.SmallInteger, index=True)
+    humidity = db.Column(db.SmallInteger, index=True)
+    record = db.Column(db.DateTime, index=True, nullable=False)
+
+
+class Gaz(db.Model):
+    __tablename__ = 'gaz'
+    id = db.Column(db.BigInteger, primary_key=True)
+    values = db.Column(db.String(48), index=True)
